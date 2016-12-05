b7b5e31a6a7810dc1644381c3d341ad017b1668f
Shubhika <sbarjaty@purdue.edu>
2016-12-05 15:57:10 -0500

    updates as of 12/5

--- ProjectsApp/forms.py
+++ ProjectsApp/forms.py
@@ -4,6 +4,8 @@ Created by Naman Patwari on 10/10/2016.
 """
 from django import forms
 
+from . import models
+
 class ProjectForm(forms.Form):
     name = forms.CharField(label='Name', max_length=30)
     description = forms.CharField(label='Description', max_length=300)
@@ -12,3 +14,45 @@ class ProjectForm(forms.Form):
     programming_language = forms.CharField(max_length=1000)
     years_of_experience = forms.CharField(max_length=1000)
     speciality = forms.CharField(max_length=1000)
+
+class AddGroupForm(forms.Form):
+    group = forms.CharField(max_length=1000)
+    project = forms.CharField(max_length=1000)
+
+class Update_Form(forms.ModelForm):
+    class Meta:
+        model = models.Project
+        fields = ('name', 'description', 'years_of_experience', 'programming_language', 'speciality')
+
+    def clean_name(self):
+        return self.initial["name"]
+
+    def clean_description(self):
+        des = self.cleaned_data.get("description")
+        # Check is email has changed
+        if des == self.initial["description"]:
+            return des
+        # Check if email exists before
+        try:
+            des = self.cleaned_data("description")
+            return des
+        except:
+            raise forms.ValidationError("There was an error, please contact us later")
+
+    def clean_years_of_experience(self):
+        years_of_experience = self.intial["years_of_experience"]
+        if years_of_experience == self.cleaned_data.get("years_of_experience"):
+            years_of_experience = self.cleaned_data.get("years_of_experience")
+        return years_of_experience
+
+    def clean_programming_language(self):
+        programming_language = self.intial["programming_language"]
+        if programming_language == self.cleaned_data.get("programming_language"):
+            programming_language = self.cleaned_data.get("programming_language")
+        return programming_language
+
+    def clean_speciality(self):
+        speciality = self.intial["speciality"]
+        if speciality == self.cleaned_data.get("speciality"):
+            speciality = self.cleaned_data.get("speciality")
+        return speciality
--- ProjectsApp/views.py
+++ ProjectsApp/views.py
@@ -7,6 +7,8 @@ from . import forms
 
 from . import models
 from . import forms
+from GroupsApp.models import Group
+
 def getProjects(request):
 	projects_list = models.Project.objects.all()
 	return render(request, 'projects.html', {
@@ -18,12 +20,22 @@ def getProject(request):
         in_name = request.GET.get('name', 'None')
         print(in_name)
         in_project = models.Project.objects.get(name__exact=in_name)
+        flag = False
+        if request.user.id == in_project.engineer_id:
+            flag = True
+        group = False
+        groups_list = Group.objects.all()
+        for in_group in groups_list:
+            if in_group.members.filter(email__exact=request.user.email).exists():
+                group = True
         context = {
             'project' : in_project,
+            'userIsMember': flag,
+            'in_group' : group,
         }
         return render(request, 'project.html', context)
-    # render error page if user is not logged in
     return render(request, 'autherror.html')
+
 def getProjectForm(request):
 	user_type = request.user.get_user_type()
 	print(user_type)
@@ -32,6 +44,52 @@ def getProjectForm(request):
 			return render(request, 'projectform.html')
 	# render error page if user is not logged in
 	return render(request, 'autherror.html')
+
+def getAddGroup(request):
+    if request.user.is_authenticated():
+            return render(request, 'addgroupform.html')
+    # render error page if user is not logged in
+    return render(request, 'autherror.html')
+
+def getAddGroupSuccess(request):
+    if request.user.is_authenticated():
+            if request.method == 'POST':
+                form = forms.AddGroupForm(request.POST)
+                print(form)
+                if form.is_valid():
+                    if Group.objects.filter(name__exact=form.cleaned_data['group']).exists():
+                        if models.Project.objects.filter(name__exact=form.cleaned_data['project']).exists() is not True:
+                            return render(request, 'autherror.html', {'error': 'project doesnt exists'})
+                        project = models.Project.objects.get(name__exact=form.cleaned_data['project'])
+                        in_group = Group.objects.get(name__exact=form.cleaned_data['group']) #check if user is a group member
+                        if in_group.project_id == None:
+                            in_group.project_id = project.id
+                            print(in_group.project_id)
+                            in_group.save()
+                            print(in_group.project_id)
+                            context = {
+                                'project': project,
+                            }
+                            return render(request, 'project.html', context)
+                        return render(request, 'addgroupform.html', {'error': 'Group has a project!! Only one project per group'})
+                    return render(request, 'addgroupform.html', {'error' : 'Group does not exisits!!!'})
+    # render error page if user is not logged in
+    return render(request, 'autherror.html')
+
+def update_project(request):
+	in_name = request.GET.get('name', 'None')
+	form = forms.Update_Form(request.POST or None, instance=request.user)
+	if form.is_valid():
+		form.save()
+		messages.success(request, 'Success, your profile was saved!')
+	user = models.Project.objects.get(name=in_name)
+	context = {
+		"form": form,
+		"page_name" : "Update",
+		"button_value" : "Update",
+		"links" : ["logout"],
+	}
+	return render(request, 'authform.html', context)
 
 def getProjectFormSuccess(request):
     if request.user.is_authenticated():
@@ -43,7 +101,7 @@ def getProjectFormSuccess(request):
                     print('heremm!!')
                     if models.Project.objects.filter(name__exact=form.cleaned_data['name']).exists():
                         return render(request, 'projectform.html', {'error' : 'Error: That Project name already exists!'})
-                    new_project = models.Project(name=form.cleaned_data['name'])
+                    new_project = models.Project(name=form.cleaned_data['name'], description=form.cleaned_data['description'], programming_language=form.cleaned_data['programming_language'], years_of_experience=form.cleaned_data['years_of_experience'], speciality=form.cleaned_data['speciality'], engineer_id= request.user.id)
                     new_project.save()
                     context = {
                         'name' : form.cleaned_data['name'],
