d28da77a520f6d4734fee16309b786ba77a16e87
j-collins <colli206@purdue.edu>
2016-12-08 00:26:38 -0500

    Do not show login if a user is already logged in.

--- CSCapstoneApp/views.py
+++ CSCapstoneApp/views.py
@@ -3,11 +3,17 @@
 Created by Harris Christiansen on 9/18/16.
 """
 from django.shortcuts import render
+from AuthenticationApp.models import MyUser
 
 def getIndex(request):
-	return render(request, 'index.html', {
-        'foo': 'bar',
-    })
+	try:
+		user_object = MyUser.objects.get(email=request.user)
+	except:
+		user_object = None
+	context = {
+		'user' : user_object,
+	}
+	return render(request, 'index.html', context)
 
 def getTable(request):
 	return render(request, 'table.html')
